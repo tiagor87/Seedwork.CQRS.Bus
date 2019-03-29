268cd04314a65a6ca6753f9fad3e8224bc8a1e09
Tiago Resende <tiagor87@gmail.com>
2019-03-29 00:34:47 -0300

    Remove unused properties

--- src/Core/ExchangeType.cs
+++ src/Core/ExchangeType.cs
@@ -9,10 +9,7 @@ namespace Seedwork.CQRS.Bus.Core
             Value = type ?? throw new ArgumentNullException(nameof(type));
         }
 
-        public static ExchangeType Fanout => new ExchangeType("fanout");
         public static ExchangeType Topic => new ExchangeType("topic");
-        public static ExchangeType Direct => new ExchangeType("direct");
-        public static ExchangeType Header => new ExchangeType("header");
 
         public string Value { get; }
     }
