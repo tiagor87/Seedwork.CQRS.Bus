35507c98bd38136f58471e74d1e5cb3060273763
Tiago Resende <tiagor87@gmail.com>
2020-04-29 17:38:42 -0300

    :art: Fix code smell

--- src/Core/Tasks.cs
+++ src/Core/Tasks.cs
@@ -54,7 +54,7 @@ namespace Seedwork.CQRS.Bus.Core
             GC.SuppressFinalize(this);
         }
 
-        private void Dispose(bool disposing)
+        protected virtual void Dispose(bool disposing)
         {
             if (_disposed)
             {
