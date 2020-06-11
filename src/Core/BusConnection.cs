68fa88baff0a6fbf9a5c78e793a0ca60dbbcb539
Tiago Resende <tiagor87@gmail.com>
2020-06-10 21:24:05 -0300

    :art: Resolve code smells

--- src/Core/BusConnection.cs
+++ src/Core/BusConnection.cs
@@ -3,7 +3,6 @@ using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Security;
-using System.Security.Authentication;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Extensions.DependencyInjection;
